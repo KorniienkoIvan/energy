5230e371f486dd896671fa66908aee80dc166426
KorniienkoIvan <kornienkoivan462@gmail.com>
2022-01-05 14:11:34 +0200

    main page line

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -1,78 +1,66 @@
 <?php 
 
 add_action( 'init', 'register_post_types' );
+function register_post_types(){
+	register_post_type( 'testimonial', [
+		'label'  => 'Testimonials',
+		'labels' => [
+			'name'               => 'Testimonials', // основное название для типа записи
+			'singular_name'      => 'Testimonial', // название для одной записи этого типа
+			'add_new'            => 'Add Testimonial', // для добавления новой записи
+			'add_new_item'       => 'Adding Testimonial', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Edit Testimonial', // для редактирования типа записи
+			'new_item'           => 'New Testimonial', // текст новой записи
+			'view_item'          => 'View Testimonial', // для просмотра записи этого типа.
+			'search_items'       => 'Search Testimonial', // для поиска по этим типам записи
+			'not_found'          => 'Testimonial not found', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Testimonial not found in the trash', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Testimonials', // название меню
+		],
+		'description'         => '',
+		'public'              => true,
+		'show_in_menu'        => true, // показывать ли в меню адмнки
+		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
+		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 8,
+		'hierarchical'        => true,
+		'supports'            => [ 'title', 'editor', 'custom-fields' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
 
-
-function register_post_types() {
-
-    /*
-       * Post Type: Testimonials.
-    */
-    $labels = [
-        "name" => __( "Testimonials", "'ikon'" ),
-        "singular_name" => __( "Testimonial", "'ikon'" ),
-    ];
-
-    $args = [
-        "label" => __( "Testimonials", "'ikon'" ),
-        "labels" => $labels,
-        "description" => "",
-        "public" => true,
-        "publicly_queryable" => true,
-        "show_ui" => true,
-        "show_in_rest" => true,
-        "rest_base" => "",
-        "rest_controller_class" => "WP_REST_Posts_Controller",
-        "has_archive" => false,
-        "show_in_menu" => true,
-        "show_in_nav_menus" => true,
-        "delete_with_user" => false,
-        "exclude_from_search" => false,
-        "capability_type" => "post",
-        "map_meta_cap" => true,
-        "hierarchical" => true,
-        "rewrite" => [ "slug" => "testimonial", "with_front" => true ],
-        "query_var" => true,
-        "menu_position" => 8,
-        "menu_icon" => "dashicons-format-chat",
-        "supports" => [ "title", "editor", "thumbnail" ],
-        "show_in_graphql" => false,
-    ];
-
-    register_post_type( "testimonial", $args );
-
-    /*
-       * Post Type: Case Study.
-    */
-    $labels = [
-        "name" => __( "Case Study", "'ikon'" ),
-        "singular_name" => __( "Case Study", "'ikon'" ),
-    ];
-
-    $args = [
-        "label" => __( "Case Study", "'ikon'" ),
-        "labels" => $labels,
-        "description" => "",
-        "public" => true,
-        "publicly_queryable" => true,
-        "show_ui" => true,
-        "show_in_rest" => true,
-        "rest_base" => "",
-        "has_archive" => false,
-        "show_in_menu" => true,
-        "show_in_nav_menus" => true,
-        "delete_with_user" => false,
-        "exclude_from_search" => false,
-        "capability_type" => "post",
-        "map_meta_cap" => true,
-        "hierarchical" => true,
-        "query_var" => true,
-        "menu_position" => 7,
-        "supports" => [ "title", "editor", "thumbnail" ],
-        "show_in_graphql" => false,
-    ];
-
-    register_post_type( "case_study", $args );
+	register_post_type( 'case_study', [
+		'label'  => 'Case Study',
+		'labels' => [
+			'name'               => 'Case Study', // основное название для типа записи
+			'singular_name'      => 'Case Study', // название для одной записи этого типа
+			'add_new'            => 'Add post', // для добавления новой записи
+			'add_new_item'       => 'Adding post', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Edit Post', // для редактирования типа записи
+			'new_item'           => 'New Post', // текст новой записи
+			'view_item'          => 'View Post', // для просмотра записи этого типа.
+			'search_items'       => 'Search Post', // для поиска по этим типам записи
+			'not_found'          => 'Post not found', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Post not found in the trash', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Case Study', // название меню
+		],
+		'description'         => '',
+		'public'              => true,
+		'show_in_menu'        => true, // показывать ли в меню адмнки
+		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
+		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 7,
+		'hierarchical'        => true,
+		'supports'            => [ 'title', 'editor', 'custom-fields', 'thumbnail' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+		'taxonomies'          => [],
+		'has_archive'         => true,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
 }
-
-add_action( 'init', 'cptui_register_my_cpts_testimonial' );
