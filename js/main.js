78fa9b9e4146dac29559778b685641a7c2ce457c
KorniienkoIvan <kornienkoivan462@gmail.com>
2022-01-10 22:23:54 +0200

    mobile single

--- js/main.js
+++ js/main.js
@@ -43,6 +43,29 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 300,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+            },
+        ]
     });
     $('.testimonial__slider').slick({
         slidesToShow: 1,
