d6903927360931b5a550bb399f79d7c93b89dc25
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2022-01-20 16:42:43 +0200

    Revert "Animations phase 1"
    This reverts commit 8d427c9e16c6c99a5736ef952388c9604e35c283.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-import $  from 'jquery';
-import { parallax } from './animations/parallax';
-import { appearence } from './animations/appearence';
-
-//Animations
-parallax();
-appearence();
+var $ = jQuery;
 
 //lazy loading
-// function lazyInit(){
-//     $('.lazy-img').Lazy({
-//         afterLoad: function(element){
-//             imagesLoaded(element,function(){
-//                 element.parent().addClass('loaded');
-//             });
-//         }
-//     });
-// }
-// lazyInit();
-
-
+function lazyInit(){
+    $('.lazy-img').Lazy({
+        afterLoad: function(element){
+            imagesLoaded(element,function(){
+                element.parent().addClass('loaded');
+            });
+        }
+    });
+}
+lazyInit();
 //smooth scroll to anchor
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
@@ -31,14 +23,14 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 //animations
 
 //appearence animation on scroll
-// $('.appear').each(function() {
-//     let el = $(this);
-//     let inview = el.waypoint(function(direction) {
-//         el.addClass('visible');
-//     }, {
-//         offset: '95%'
-//     });
-// });
+$('.appear').each(function() {
+    let el = $(this);
+    let inview = el.waypoint(function(direction) {
+        el.addClass('visible');
+    }, {
+        offset: '95%'
+    });
+});
 
 $(document).ready(function(){
     //Single Page Slider
