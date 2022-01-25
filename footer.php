cbc907f09c3b66dbf3f037eaa02e46c9b08fb269
Vitalii Omelchenko <vomelchenko@purpleplanet.com>
2022-01-25 17:43:25 +0200

    Footer privacy fix

--- footer.php
+++ footer.php
@@ -4,6 +4,14 @@
 		<a href="#main" class="back_to_top_button">
             Back to top
         </a>
+
+		<?php if (get_privacy_policy_url()) : ?>
+
+			<a href="<?php echo get_privacy_policy_url();?>" class="footer__privacy">
+				Privacy Policy
+			</a>
+		<?php endif; ?>
+
 	</footer>
 
 	<?php wp_footer(); ?>
